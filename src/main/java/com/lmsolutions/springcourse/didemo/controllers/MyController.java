c9e8a7699ed183bb3d0b026a0b5c649e34e736b8
Laurent Morissette <laurent.morissette@gmail.com>
2018-12-21 22:29:36 -0500

    intro to dependency injection

---
+++ src/main/java/com/lmsolutions/springcourse/didemo/controllers/MyController.java
@@ -0,0 +1,10 @@
+package com.lmsolutions.springcourse.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public void hello(){
+        System.out.println("Hello from  MyController in di-0demo");
+    }
+}
